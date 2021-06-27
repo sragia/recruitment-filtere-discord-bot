990a128a3caeaccd743dc6d11dcff3b05b3924e1
Aigars Bedeicis <a.bedeicis@gmail.com>
2021-06-27 22:42:40 +0300

    Initial commit

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,21 @@
 import "reflect-metadata";
 import { Client } from "@typeit/discord";
+import { config } from "dotenv";
+config();
 
 export class Main {
   static start() {
-    const client = new Client();
+    const client = new Client({
+     
+    });
+
+    console.log(process.env.BOT_TOKEN);
+    client.once('ready', () => {
+      console.log('ready');
+    })
+    
     client.login(
-      "YOUR_TOKEN",
+      process.env.BOT_TOKEN,
       `${__dirname}/discord/*.ts`
     );
   }
--- src/discord/DiscordBot.ts
+++ src/discord/DiscordBot.ts
@@ -1,5 +1,68 @@
-import { Discord } from "@typeit/discord";
+import { Client, Command, CommandMessage, Discord, On } from "@typeit/discord";
+import { Message, MessageEmbed, MessageEmbedField } from "discord.js";
 
-@Discord()
+const channelToWatch = process.env.WATCH_CHANNEL;
+const channelToSend = process.env.SEND_CHANNEL;
+const KILLED = process.env.MIN_KILLED;
+
+@Discord({prefix: '#'})
 export abstract class DiscordBot {
+
+    @Command('hello')
+    private hello(message: CommandMessage) {
+        message.reply('sup');
+    }
+
+    private sendMessageToChannel(message: any, client: Client)
+    {
+        const channel = message.guild.channels.find(channel => channel.id === channelToSend);
+
+        if (channel) {  
+            const msg = new MessageEmbed(
+                new Message(channel, {}, client),
+                message.embeds[0]
+            ); 
+            const embed = message.embeds[0];
+
+            msg.fields = embed.fields;
+            msg.description = embed.description;
+            msg.thumbnail = embed.thumbnail;
+            msg.timestamp = embed.timestamp;
+            msg.type = embed.type;
+            msg.title = embed.title;
+
+            channel.send({embed: msg});
+        }
+    }
+
+    private validateMessage(raidProg: MessageEmbedField)
+    {
+        try {
+            const prog = raidProg.value.split(':**')[1].trim();
+            const curr = prog.split('/')[0];
+            const diff = prog.split(' ')[1];
+
+            return diff === 'M' && parseInt(curr, 10) > parseInt(KILLED, 10);
+        } catch(e) {
+            // fail silently-ish;
+            console.error(e);
+        }
+    }
+
+    @On("message")
+    private onMessage(
+        message: any,
+        client: Client
+   ) {
+       if (message.channel.id === channelToWatch) {
+           if (message.embeds && message.embeds.length) {
+                const raidProg = message.embeds[0].fields.find(field => field.name === '__Recent Raid Progression__')
+                if (this.validateMessage(raidProg)) {
+                    this.sendMessageToChannel(message, client);
+                }
+           }
+       }
+    }
+
+
 }
